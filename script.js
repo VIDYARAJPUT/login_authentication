21ba401b26aa5202f44822cfee484e601fa25fe7
VIDYARAJPUT <108059827+VIDYARAJPUT@users.noreply.github.com>
2022-07-30 13:44:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function login()       
+             {
+                 var uname = document.getElementById('username').value;
+                 var pass = document.getElementById('password').value;
+             
+                 if( uname == "newton" && pass == "123")
+                 {
+                   alert("login successful!");
+                 }
+                 else
+                 {
+                     alert("login failed!");
+                 }
+             }
