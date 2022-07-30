e8792c1a6f873655b859535915355b8f43b07ef7
VIDYARAJPUT <108059827+VIDYARAJPUT@users.noreply.github.com>
2022-07-30 13:46:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ function login()
              {
                  var uname = document.getElementById('username').value;
                  var pass = document.getElementById('password').value;
-             
-                 if( uname == "newton" && pass == "123")
+                     if( uname == "newton" && pass == "123")
                  {
                    alert("login successful!");
                  }
